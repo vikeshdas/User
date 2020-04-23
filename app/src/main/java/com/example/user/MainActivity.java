7eac6b8b438f52802ea1d3536212a59b6db7fc18
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-04-23 13:29:57 +0530

    Fixed User Registration and Login of Firebase using email and password

--- app/src/main/java/com/example/user/MainActivity.java
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+FirebaseAuth mAuth;
+TextView name,email,uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        uid=findViewById(R.id.uid);
+
+        name.setText(mAuth.getCurrentUser().getDisplayName());
+        uid.setText(mAuth.getCurrentUser().getUid());
+        email.setText(mAuth.getCurrentUser().getEmail());
+
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.getInstance().signOut();
+                Intent intent=new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user/SignIn.java
+++ app/src/main/java/com/example/user/SignIn.java
@@ -1,14 +1,72 @@
 package com.example.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
-
+  EditText email,password;
+  FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        email=findViewById(R.id.editText);
+        password=findViewById(R.id.editText2);
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.textView2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SignIn.this,SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignIn.this,"click",Toast.LENGTH_SHORT).show();
+                login();
+            }
+        });
+
+    }
+
+    private void login() {
+
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent=new Intent(SignIn.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(SignIn.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
